b3460e2c2c9e83933385aac2df9d130f1714015b
jrehm135 <jrehm135@hotmail.com>
2024-10-15 15:44:27 -0400

    Removed create-react-context and replaced node-sass with newer sass package

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,16 @@ const config = {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader','css-loader','sass-loader']
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              api: "modern"
+            },
+          }
+        ]
       },
       {
         test: /\.(js|jsx)$/,
